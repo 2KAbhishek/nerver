5ffa7777d05ab13ac8cd05c7187be1884336f5aa
Abhishek Keshri <keshri.abhishek63@gmail.com>
2020-05-09 00:41:14 +0530

    Add filePath var to store file being served currently.

--- index.js
+++ index.js
@@ -3,7 +3,36 @@ const path = require("path");
 const fs = require("fs");
 
 const server = http.createServer((req, res) => {
+  let filePath = path.join(
+    __dirname,
+    "public",
+    req.url === "/" ? "index.html" : req.url
+  );
 
+  let extName = path.extname(filePath);
+  let contentType = "text/html";
+
+  switch(extName){
+    // Add more extenstions if needed
+    case ".js":
+      contentType = "text/javascript";
+      break;
+    case ".css":
+      contentType = "text/css";
+      break;
+    case ".json":
+      contentType = "application/json";
+      break;
+    case ".png":
+      contentType = "image/png";
+      break;
+    case ".jpg":
+      contentType = "image/jpg";
+      break;
+  }
+
+  if (contentType == "text/html" && extName == "") filePath += ".html";
+  console.log(`Serving ${filePath}`);
 
 });
 
